e3b5ef42704dc661de860a6a79539fcf1096c6b0
Krzysztof Witkowski <krzysiekwit@gmail.com>
2020-04-05 21:55:36 +0200

    added menu chaning function

--- car-dealership-project/setup/js/script.js
+++ car-dealership-project/setup/js/script.js
@@ -1,3 +1,24 @@
 window.addEventListener("load", () =>
   document.querySelector(".preloader").classList.add("hidePreloader")
 );
+// Filter
+const FilterCars = (() => {
+  const filter = document.querySelectorAll(".filter-btn");
+  console.log(filter);
+  filter.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      const value = event.target.dataset.filter;
+      const singleCar = document.querySelectorAll(".single-car");
+
+      singleCar.forEach((car) => {
+        if (value === "all") {
+          car.style.display = "block";
+        } else {
+          !car.classList.contains(value)
+            ? (car.style.display = "none")
+            : (car.style.display = "block");
+        }
+      });
+    });
+  });
+})();
